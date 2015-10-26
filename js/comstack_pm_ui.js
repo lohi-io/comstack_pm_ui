cc6738a15db80f5d1dcd4e21b325127305eaf1b4
Alli <allister.price@gmail.com>
2015-10-26 15:16:34 +0000

    Only use tooltips if the jquery tooltip plugin is present.

--- js/comstack_pm_ui.js
+++ js/comstack_pm_ui.js
@@ -1,7 +1,7 @@
 (function($) {
   Drupal.behaviors.comstack_pm_ui = {
     attach: function(context, settings) {
-      if (typeof Drupal.settings.comstack_pm_ui.tooltips != 'undefined') {
+      if (typeof Drupal.settings.comstack_pm_ui.tooltips != 'undefined' && $.isFunction($.fn.tooltip)) {
         $('[data-toggle="tooltip"]', context).tooltip();
       }
     }
